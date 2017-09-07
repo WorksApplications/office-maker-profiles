7602fcc2236fc312cd2c1557adea7d8c9b943b86
torii_y <torii_y@worksap.co.jp>
2017-09-07 16:34:30 +0900

    add scheduled scaledown operation

---
+++ functions/operation/scaledown.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk');
+const options = require('../common/db-options.js');
+const documentClient = new AWS.DynamoDB.DocumentClient(options);
+const dynamoUtil = require('../common/dynamo-util.js');
+const log = require('../common/log.js');
+
+exports.handler = () => {
+  putAndDelete('profiles', {
+    userId: 'scaledown@worksap.co.jp'
+  }, {
+    userId: 'scaledown@worksap.co.jp'
+  }).then(_ => {
+    return putAndDelete('profilesPosts', {
+      key: 'scaledown',
+      name: 'scaledown'
+    }, {
+      key: 'scaledown',
+      name: 'scaledown'
+    });
+  }).then(_ => {
+    return putAndDelete('profilesSearchHelp', {
+      key: 'scaledown',
+      userId: 'scaledown'
+    }, {
+      key: 'scaledown',
+      userId: 'scaledown'
+    });
+  }).then(_ => {
+    console.log('done.');
+  });
+};
+
+function putAndDelete(tableName, putItem, deleteKey) {
+  return dynamoUtil.put({
+    TableName: tableName,
+    Item: putItem,
+  }).catch(e => {
+    console.log(e);
+  }).then(_ => {
+    return dynamoUtil.delete({
+      TableName: tableName,
+      Key: deleteKey
+    });
+  }).catch(e => {
+    console.log(e);
+  });
+}
